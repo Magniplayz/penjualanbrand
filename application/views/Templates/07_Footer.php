dcdd3270bbd7b4163790a92c16058bcec2b88fe9
Al Ghifari Anwar <falyoseblings@gmail.com>
2021-06-08 16:49:57 +0700

    Footer

--- application/views/Templates/07_Footer.php
+++ application/views/Templates/07_Footer.php
@@ -2,10 +2,10 @@
  <footer class="main-footer">
      <!-- To the right -->
      <div class="float-right d-none d-sm-inline">
-         Anything you want
+         Penjualan Brand
      </div>
      <!-- Default to the left -->
-     <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+     <strong>Copyright &copy; 2021 <a href="#">Kelompok B02</a>.</strong> All rights reserved.
  </footer>
  </div>
  <!-- ./wrapper -->
